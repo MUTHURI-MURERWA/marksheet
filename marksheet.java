8469b66f2184fcf1d1349fbc787691e676a4bba0
MUTHURI-MURERWA <denismuthuri999@gmail.com>
2025-11-26 13:58:31 +0300

    Add files via upload

---
+++ marksheet.java
@@ -0,0 +1,202 @@
+import java.util.Scanner;
+public class marksheet {
+    public static void main(String[] args) {
+        System.out.println("Dedan Kimathi University of Technology");
+        System.out.println("  School of Computer Science and IT");
+        System.out.println("   Department of Computer Science");
+        System.out.println("    Academic Year: 2024/2025");
+        System.out.println("STUDENT MARKSHEET (SECOND YEAR RESULTS)");
+        System.out.println("---------------------------------------------------");
+
+        Scanner sc = new Scanner(System.in);
+
+        // Student 1
+        String regNo1, name1;
+        int unit1_1, unit1_2, unit1_3, unit1_4, unit1_5, unit1_6, unit1_7;
+        int total1;
+        double average1;
+
+
+        System.out.print("Enter Full Name for Student 1: ");
+        name1 = sc.nextLine();
+        System.out.print("Enter Registration Number for Student 1: ");
+        regNo1 = sc.nextLine();
+
+
+
+        System.out.print("Enter marks for Unit 1: ");
+        unit1_1 = sc.nextInt();
+        System.out.print("Enter marks for Unit 2: ");
+        unit1_2 = sc.nextInt();
+        System.out.print("Enter marks for Unit 3: ");
+        unit1_3 = sc.nextInt();
+        System.out.print("Enter marks for Unit 4: ");
+        unit1_4 = sc.nextInt();
+        System.out.print("Enter marks for Unit 5: ");
+        unit1_5 = sc.nextInt();
+        System.out.print("Enter marks for Unit 6: ");
+        unit1_6 = sc.nextInt();
+        System.out.print("Enter marks for Unit 7: ");
+        unit1_7 = sc.nextInt();
+        sc.nextLine();
+
+        total1 = unit1_1 + unit1_2 + unit1_3 + unit1_4 + unit1_5 + unit1_6 + unit1_7;
+        average1 = total1 / 7.0;
+
+        // Student 2
+
+        String regNo2, name2;
+        int unit2_1, unit2_2, unit2_3, unit2_4, unit2_5, unit2_6, unit2_7;
+        int total2;
+        double average2;
+
+
+        System.out.print("Enter Full Name for Student 2: ");
+        name2 = sc.nextLine();
+        System.out.print("Enter Registration Number for Student 2: ");
+        regNo2 = sc.nextLine();
+
+
+
+        System.out.print("Enter marks for Unit 1: ");
+        unit2_1 = sc.nextInt();
+        System.out.print("Enter marks for Unit 2: ");
+        unit2_2 = sc.nextInt();
+        System.out.print("Enter marks for Unit 3: ");
+        unit2_3 = sc.nextInt();
+        System.out.print("Enter marks for Unit 4: ");
+        unit2_4 = sc.nextInt();
+        System.out.print("Enter marks for Unit 5: ");
+        unit2_5 = sc.nextInt();
+        System.out.print("Enter marks for Unit 6: ");
+        unit2_6 = sc.nextInt();
+        System.out.print("Enter marks for Unit 7: ");
+        unit2_7 = sc.nextInt();
+        sc.nextLine();
+
+
+        total2 = unit2_1 + unit2_2 + unit2_3 + unit2_4 + unit2_5 + unit2_6 + unit2_7;
+        average2 = total2 / 7.0;
+
+
+
+        // Student 3
+        String regNo3, name3;
+        int unit3_1, unit3_2, unit3_3, unit3_4, unit3_5, unit3_6, unit3_7;
+        int total3;
+        double average3;
+
+
+        System.out.print("Enter Full Name for Student 3: ");
+        name3 = sc.nextLine();
+        System.out.print("Enter Registration Number for Student 3: ");
+        regNo3 = sc.nextLine();
+
+
+        System.out.print("Enter marks for Unit 1: ");
+        unit3_1 = sc.nextInt();
+        System.out.print("Enter marks for Unit 2: ");
+        unit3_2 = sc.nextInt();
+        System.out.print("Enter marks for Unit 3: ");
+        unit3_3 = sc.nextInt();
+        System.out.print("Enter marks for Unit 4: ");
+        unit3_4 = sc.nextInt();
+        System.out.print("Enter marks for Unit 5: ");
+        unit3_5 = sc.nextInt();
+        System.out.print("Enter marks for Unit 6: ");
+        unit3_6 = sc.nextInt();
+        System.out.print("Enter marks for Unit 7: ");
+        unit3_7 = sc.nextInt();
+        sc.nextLine();
+
+        total3 = unit3_1 + unit3_2 + unit3_3 + unit3_4 + unit3_5 + unit3_6 + unit3_7;
+        average3 = total3 / 7.0;
+
+
+        // Student 4
+        String regNo4, name4;
+        int unit4_1, unit4_2, unit4_3, unit4_4, unit4_5, unit4_6, unit4_7;
+        int total4;
+        double average4;
+
+
+        System.out.print("Enter Full Name for Student 4: ");
+        name4 = sc.nextLine();
+        System.out.print("Enter Registration Number for Student 4: ");
+        regNo4 = sc.nextLine();
+
+
+        System.out.print("Enter marks for Unit 1: ");
+        unit4_1 = sc.nextInt();
+        System.out.print("Enter marks for Unit 2: ");
+        unit4_2 = sc.nextInt();
+        System.out.print("Enter marks for Unit 3: ");
+        unit4_3 = sc.nextInt();
+        System.out.print("Enter marks for Unit 4: ");
+        unit4_4 = sc.nextInt();
+        System.out.print("Enter marks for Unit 5: ");
+        unit4_5 = sc.nextInt();
+        System.out.print("Enter marks for Unit 6: ");
+        unit4_6 = sc.nextInt();
+        System.out.print("Enter marks for Unit 7: ");
+        unit4_7 = sc.nextInt();
+        sc.nextLine();
+
+        total4 = unit4_1 + unit4_2 + unit4_3 + unit4_4 + unit4_5 + unit4_6 + unit4_7;
+        average4 = total4 / 7.0;
+
+
+        // STUDENT 5
+        String regNo5, name5;
+        int unit5_1, unit5_2, unit5_3, unit5_4, unit5_5, unit5_6, unit5_7;
+        int total5;
+        double average5;
+
+
+        System.out.print("Enter Full Name for Student 5: ");
+        name5 = sc.nextLine();
+        System.out.print("Enter Registration Number for Student 5: ");
+        regNo5 = sc.nextLine();
+
+
+        System.out.print("Enter marks for Unit 1: ");
+        unit5_1 = sc.nextInt();
+        System.out.print("Enter marks for Unit 2: ");
+        unit5_2 = sc.nextInt();
+        System.out.print("Enter marks for Unit 3: ");
+        unit5_3 = sc.nextInt();
+        System.out.print("Enter marks for Unit 4: ");
+        unit5_4 = sc.nextInt();
+        System.out.print("Enter marks for Unit 5: ");
+        unit5_5 = sc.nextInt();
+        System.out.print("Enter marks for Unit 6: ");
+        unit5_6 = sc.nextInt();
+        System.out.print("Enter marks for Unit 7: ");
+        unit5_7 = sc.nextInt();
+
+        total5 = unit5_1 + unit5_2 + unit5_3 + unit5_4 + unit5_5 + unit5_6 + unit5_7;
+        average5 = total5 / 7.0;
+
+        System.out.println("\n\n Student Score Sheet                                                                               ");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf(" %-20s  | %-30s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | %-6s | %-7s | %-7s | %-8s | %-5s | \n",
+                "Reg No.", "Full Name", "CCS2211", "CCS2212", "CCS2213", "CCS2214", "CCS2215", "CCS2216", "CCS2217", "TOTALS", "AVERAGE", "STATUS", "GRADE");
+        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        System.out.printf("| %-20s | %-30s | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-7d | %-7.2f | %-8s | %-5s |\n",
+                regNo1, name1, unit1_1, unit1_2, unit1_3, unit1_4, unit1_5, unit1_6, unit1_7, total1, average1, "", "");
+
+        System.out.printf("| %-20s | %-30s | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-7d | %-7.2f | %-8s | %-5s |\n",
+                regNo2, name2, unit2_1, unit2_2, unit2_3, unit2_4, unit2_5, unit2_6, unit2_7, total2, average2, "", "");
+
+        System.out.printf("| %-20s | %-30s | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-7d | %-7.2f | %-8s | %-5s |\n",
+                regNo3, name3, unit3_1, unit3_2, unit3_3, unit3_4, unit3_5, unit3_6, unit3_7, total3, average3, "", "");
+
+        System.out.printf("| %-20s | %-30s | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-7d | %-7.2f | %-8s | %-5s |\n",
+                regNo4, name4, unit4_1, unit4_2, unit4_3, unit4_4, unit4_5, unit4_6, unit4_7, total4, average4, "", "");
+
+        System.out.printf("| %-20s | %-30s | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-6d | %-7d | %-7.2f | %-8s | %-5s |\n",
+                regNo5, name5, unit5_1, unit5_2, unit5_3, unit5_4, unit5_5, unit5_6, unit5_7, total5, average5, "", "");
+
+        sc.close();
+    }
+}
